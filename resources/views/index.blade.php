9d439b3dfb8e1fb6ae8d7508f057da40d3431896
Darma Wiryanata <darmawiryanata@gmail.com>
2020-07-26 00:01:36 +0800

    Login modal center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -177,7 +177,7 @@
 
     <div class="modal fade text-left" id="modal" tabindex="-1" role="dialog"
     aria-labelledby="myModalLabel1" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-dialog modal-dialog-centered justify-content-center" role="document">
             <div class="modal-content modal-xs">
                 <div class="modal-header">
                     <h4 class="modal-title" id="myModalLabel1">Masuk</h4>
